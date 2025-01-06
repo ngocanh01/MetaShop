6f47e6c8c2ae9b2bf3f72fa4908e8133afdeca03
ngocanh01 <nanh5605@gmail.com>
2025-01-06 16:33:19 +0700

    Update Cart

--- MetaShop.Web/Controllers/CartController.cs
+++ MetaShop.Web/Controllers/CartController.cs
@@ -2,7 +2,6 @@
 using MetaShop.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Collections.Generic;
 
 namespace MetaShop.Web.Controllers
 {
@@ -19,19 +18,20 @@ namespace MetaShop.Web.Controllers
 		}
 		[Route("")]
 		[Route("index")]
-		public async Task<IActionResult> Index(ProductCartInfoDtos product)
+		public async Task<IActionResult> Index()
 		{
-			List<ProductCartInfoDtos> products = new List<ProductCartInfoDtos>();
-			if (HttpContext.Session.GetString("myCart") != null)
-			{
-				products = JsonConvert.DeserializeObject<List<ProductCartInfoDtos>>(HttpContext.Session.GetString("myCart"));
-			} 
+			// Retrieve the cart from the session or initialize a new one if it's empty
+			var cart = HttpContext.Session.GetString("myCart") != null
+				? JsonConvert.DeserializeObject<List<ProductCartInfoDtos>>(HttpContext.Session.GetString("myCart"))
+				: new List<ProductCartInfoDtos>();
 
+			// Prepare the view model
 			var model = new CartViewModel
 			{
 				Categories = await _categoryService.GetAllAsync(),
 				Products = await _productService.GetAllAsync(),
-				ProductsCart = products
+				ProductsCart = cart,
+				CartTotal = cart.Sum(x => x.Total) // Calculate total for the cart
 			};
 
 			return View("Index", model);
@@ -40,34 +40,87 @@ namespace MetaShop.Web.Controllers
 		[HttpPost("addToCart")]
 		public async Task<IActionResult> AddToCart(ProductCartInfoDtos product)
 		{
-			List<ProductCartInfoDtos> products = new List<ProductCartInfoDtos>();
-
-			product.Total = product.Quantity * product.Price;
-
-			if (HttpContext.Session.GetString("myCart") != null)
+			if (product == null || product.Quantity <= 0)
 			{
-				products = JsonConvert.DeserializeObject<List<ProductCartInfoDtos>>(HttpContext.Session.GetString("myCart"));
-
-				var productItem = products.FirstOrDefault(x => x.Id == product.Id);
-				if (productItem != null)
-				{
-					product.Quantity += productItem.Quantity;
-					product.Total += productItem.Total;
-					products.Remove(productItem);
-				}
+				ModelState.AddModelError("", "Invalid product or quantity.");
+				return RedirectToAction("Index");
 			}
 
-			products.Add(product);
-			HttpContext.Session.SetString("myCart", JsonConvert.SerializeObject(products));
+			var cart = HttpContext.Session.GetString("myCart") != null
+				? JsonConvert.DeserializeObject<List<ProductCartInfoDtos>>(HttpContext.Session.GetString("myCart"))
+				: new List<ProductCartInfoDtos>();
 
+			var existingProduct = cart.FirstOrDefault(x => x.Id == product.Id);
+			if (existingProduct != null)
+			{
+				// Update the quantity and total for the existing product
+				existingProduct.Quantity += product.Quantity;
+				existingProduct.Total = existingProduct.Quantity * existingProduct.Price;
+			}
+			else
+			{
+				// Add the new product to the cart
+				product.Total = product.Quantity * product.Price;
+				cart.Add(product);
+			}
+
+			// Update the cart in session
+			HttpContext.Session.SetString("myCart", JsonConvert.SerializeObject(cart));
+
+			// Prepare the view model
 			var model = new CartViewModel
 			{
 				Categories = await _categoryService.GetAllAsync(),
 				Products = await _productService.GetAllAsync(),
-				ProductsCart = JsonConvert.DeserializeObject<List<ProductCartInfoDtos>>(HttpContext.Session.GetString("myCart"))
+				ProductsCart = cart,
+				CartTotal = cart.Sum(x => x.Total)
 			};
 
 			return View("Index", model);
+		}
+
+
+		[HttpPost("updateCart")]
+		public async Task<IActionResult> UpdateCart(List<ProductCartInfoDtos> updatedCartItems)
+		{
+			if (updatedCartItems == null || !updatedCartItems.Any())
+			{
+				ModelState.AddModelError("", "No items in the cart.");
+				return RedirectToAction("Index"); // Or return the same view with an error message
+			}
+
+			foreach (var item in updatedCartItems)
+			{
+				if (item.Quantity <= 0)
+				{
+					updatedCartItems.Remove(item);
+					ModelState.AddModelError("", "Quantity must be greater than zero.");
+				}
+			}
+
+			if (!ModelState.IsValid)
+			{
+				// Load necessary data for the cart view
+				var model = new CartViewModel
+				{
+					Categories = await _categoryService.GetAllAsync(),
+					Products = await _productService.GetAllAsync(),
+					ProductsCart = JsonConvert.DeserializeObject<List<ProductCartInfoDtos>>(HttpContext.Session.GetString("myCart")),
+					CartTotal = updatedCartItems.Sum(x => x.Price * x.Quantity)
+				};
+
+				return View("Index", model);
+			}
+
+			// Update logic
+			foreach (var product in updatedCartItems)
+			{
+				product.Total = product.Price * product.Quantity;
+			}
+
+			HttpContext.Session.SetString("myCart", JsonConvert.SerializeObject(updatedCartItems));
+
+			return RedirectToAction("Index");
 		}
 	}
 }
